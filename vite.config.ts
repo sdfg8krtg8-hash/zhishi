5f0369b35059b784358f3fa172dd5ca43f0fa0a8
Developer <dev@zhishi.local>
2026-05-31 21:18:07 +0800

    fix: base改为/，添加vercel.json SPA路由配置，修复Vercel 404

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/zhishi/',
+  base: '/',
   plugins: [
     vue(),
     vueJsx(),
